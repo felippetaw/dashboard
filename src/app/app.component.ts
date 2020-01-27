557aa2d3fd141edef0b010a01d31580934b10bcf
Felippe <felippetaw@gmail.com>
2020-01-27 19:13:09 -0300

    dashboard app with data.json integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import  content  from '../assets/data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dashboard';
+export class AppComponent implements OnInit{
+  title = 'Dashboard';
+  public contents = content.contents;
+  keys:any[] = [];
+  data:any;
+  showModal: boolean;
+  content: string;
+  titleModal: string;
+  
+  getData() {
+    this.contents.forEach(element => {
+      element.Courses.forEach(e => {
+        e.Company = element.Company
+        this.keys.push(e);
+      });
+    });
+    this.keys.sort((a, b) => a.Company > b.Company ? 1 : -1);
+    this.keys = this.keys.filter(i => i.Status == 'Active');
+  }
+  ngOnInit() {
+    this.getData();
+    console.log(this.keys);
+    console.log(this.contents);
+  } 
+  
+  sortCompany() {
+    this.keys.sort((a, b) => a.Company > b.Company ? 1 : -1);
+  }
+
+  sortQuantity(boo) {
+    if(boo == true) {
+      this.keys.sort((a, b) => parseFloat(a.Quantity) > parseFloat(b.Quantity) ? 1 : -1);
+    } else if (boo == false){
+      this.keys.sort((a, b) => parseFloat(a.Quantity) > parseFloat(b.Quantity) ? -1 : 1);
+    }
+  }
+
+  search() {
+    let input, filter, table, tr, td, i, txtValue;
+
+    input = document.getElementById("input");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tab");
+    tr = table.getElementsByTagName("tr");
+    
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }        
+    }
+  }
+
+  openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+
+  show(message) {
+    this.showModal = true; 
+    this.content = message; 
+    this.titleModal = "Descrição do curso";    
+  }
+  hide() {
+    this.showModal = false;
+  }
+
+  
 }
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+  closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+}
